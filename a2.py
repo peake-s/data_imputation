28df5e9f86e381725dc61cf9e3b50465f10e20de
Sammy Peake <peakes@vcu.edu>
2022-09-23 09:56:01 -0400

    success

--- a2.py
+++ a2.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import time
 
+from pytools import P
+
 class imputers:
     
     def __init__(self, filename):
@@ -67,17 +69,18 @@ class imputers:
         return locations
         
     def _find_missing(self):
-        missing = []
+        missing_pos = []
         missing_rows = []
         for (idx,col) in enumerate(self.df_vec):
             for (i,val) in enumerate(col):
                 if val == '?':
                     self.df_vec[idx][i] = 1.0 
-                    missing.append((idx,i))
+                    missing_pos.append(i)
                     missing_rows.append(i)
-                self.df_vec[idx][i] = float(self.df_vec[idx][i])
+                    self.num_imputed += 1
 
-        return (missing, missing_rows)
+                self.df_vec[idx][i] = float(self.df_vec[idx][i])
+        return (missing_pos, missing_rows)
 
     def _to_vec(self):
         df_vec = self.df.to_numpy()
@@ -85,18 +88,20 @@ class imputers:
         #    df_vec[col] = self.df[col].to_numpy()
         return df_vec
 
-    def _manhattan_distance(self,r, idx,dfvec):
-        #compare values across all objects
-        distances = []
-        
-        #loop over all rows except for the one passed in
+    def _manhattan_distance(self,r, idx,idx_c,dfvec):
+        #compare values across all objects]
+        current = 1000
+        val_to_replace = 0
+        #loop over all rows except the one passed in
         for (i,row) in enumerate(dfvec):
             if i == idx:
                 next
             distance = sum(abs(np.subtract(row,r)))
-            distances.append(distance)
-
-        return min(distances)
+            if distance < current:
+                current = distance
+                val_to_replace = dfvec[i][idx_c]
+        print(val_to_replace)
+        return val_to_replace
 
     def _hot_deck_imputation(self):
         start = time.time()
@@ -108,14 +113,14 @@ class imputers:
         #iterate over rows
         for (idx,row) in enumerate(self.df_vec):
             if idx in missing_rows:
-                self.df_vec[idx] = self._manhattan_distance(row, idx,self.df_vec)
+                self.df_vec[idx][locations[idx]] = self._manhattan_distance(row, idx,locations[idx],self.df_vec)
 
         x = list(self.df)
         self.df = pd.DataFrame(self.df_vec,columns=x)
 
         end = time.time()
         self.run_time = (end - start) * 1000
-        print(self.run_time)
+
         return locations
  
     def sv(self, filename): 
@@ -137,12 +142,14 @@ class imputers:
         self.sv(fname)
 
 def main():
-    #imp_mean_01 = imputers('dataset_missing01.csv')
-    #imp_mean_01.impute(type = 'mean',missing = '01')
-    #imp_mean_10 = imputers('dataset_missing10.csv')
-    #imp_mean_10.impute(type = 'mean',missing = '10')
+    imp_mean_01 = imputers('dataset_missing01.csv')
+    imp_mean_01.impute(type = 'mean',missing = '01')
+    imp_mean_10 = imputers('dataset_missing10.csv')
+    imp_mean_10.impute(type = 'mean',missing = '10')
     imp_hd_01 = imputers('dataset_missing01.csv')
     imp_hd_01.impute(type = 'hd',missing = '01')
+    imp_hd_10 = imputers('dataset_missing10.csv')
+    imp_hd_10.impute(type = 'hd',missing = '10')
 
 if __name__ == '__main__':
     main()
