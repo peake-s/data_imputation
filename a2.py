fc547ee713245489cf14066a552c899483b7a804
Sammy Peake <peakes@vcu.edu>
2022-09-23 11:54:28 -0400

    hmm:

--- a2.py
+++ a2.py
@@ -18,6 +18,7 @@ class imputers:
     def _mae_calc(self,locations):
         #extract locations of imputed values and compare
         for col in self.complete_df:
+            
             for idx in locations:
                 #loop over the array of locations
                 self.mae += abs(self.df[col][idx] - self.complete_df[col][idx]) 
@@ -71,16 +72,20 @@ class imputers:
     def _find_missing(self):
         missing_pos = []
         missing_rows = []
+        indexes = []
         for (idx,col) in enumerate(self.df_vec):
             for (i,val) in enumerate(col):
                 if val == '?':
                     self.df_vec[idx][i] = 1.0 
-                    missing_pos.append(i)
-                    missing_rows.append(i)
+                    missing_pos.append((idx,i))
+                    missing_rows.append(idx)
                     self.num_imputed += 1
+                    indexes.append(i)
 
                 self.df_vec[idx][i] = float(self.df_vec[idx][i])
-        return (missing_pos, missing_rows)
+                
+        #print(missing_pos[0])
+        return (missing_pos, missing_rows,indexes)
 
     def _to_vec(self):
         df_vec = self.df.to_numpy()
@@ -88,32 +93,56 @@ class imputers:
         #    df_vec[col] = self.df[col].to_numpy()
         return df_vec
 
-    def _manhattan_distance(self,r, idx,idx_c,dfvec):
+    def _manhattan_distance(self,r, idx,dfvec):
         #compare values across all objects]
         current = 1000
-        val_to_replace = 0
+        #row to get values from
+        loc = 0
         #loop over all rows except the one passed in
         for (i,row) in enumerate(dfvec):
             if i == idx:
-                next
+                continue
+            if i == idx:
+                print("why ere")
             distance = sum(abs(np.subtract(row,r)))
+
             if distance < current:
                 current = distance
-                val_to_replace = dfvec[i][idx_c]
-        print(val_to_replace)
-        return val_to_replace
+                loc = i
+                #print(loc)
+    
+        #val to replace is the lowest sum (closest row) 
+        #print(loc)
+        return loc
 
     def _hot_deck_imputation(self):
         start = time.time()
         #indexes of the missing values
         self.df_vec = self._to_vec()
-        locations,missing_rows = self._find_missing()
-        
-        
+        cp = self.df_vec
+        locations,missing_rows,indexes = self._find_missing()
         #iterate over rows
+        #print(locations[0])
         for (idx,row) in enumerate(self.df_vec):
+            
             if idx in missing_rows:
-                self.df_vec[idx][locations[idx]] = self._manhattan_distance(row, idx,locations[idx],self.df_vec)
+                closest_row_idx = self._manhattan_distance(row, idx,self.df_vec)
+                #print(f"cri {closest_row_idx} {df_vec[idx]})
+                for (j,val) in enumerate(self.df_vec[idx]):
+                    #print(f"cri {closest_row_idx} current row: {idx} cri val {self.df_vec[closest_row_idx][j]}")
+                    #loop thru idx of row with missing vals
+                    te = self.df_vec[closest_row_idx][j]
+                    #print(f"{idx},{j} te: {te}")
+                    if cp[idx][j] == '?':
+                        print(f"te in {te}")
+                        print(self.df_vec[closest_row_idx][j])
+                        self.df_vec[idx][j] = self.df_vec[closest_row_idx][j]
+            
+
+
+        #For missing row find values that are closest
+        #for (idx,r) in enumerate(missing_rows):
+        #    distances.append(self._manhattan_distance(self.df_vec[idx], idx,locations[0][idx],self.df_vec))
 
         x = list(self.df)
         self.df = pd.DataFrame(self.df_vec,columns=x)
@@ -121,7 +150,7 @@ class imputers:
         end = time.time()
         self.run_time = (end - start) * 1000
 
-        return locations
+        return indexes
  
     def sv(self, filename): 
         self.df.to_csv(filename,index=False)
@@ -142,10 +171,10 @@ class imputers:
         self.sv(fname)
 
 def main():
-    imp_mean_01 = imputers('dataset_missing01.csv')
-    imp_mean_01.impute(type = 'mean',missing = '01')
-    imp_mean_10 = imputers('dataset_missing10.csv')
-    imp_mean_10.impute(type = 'mean',missing = '10')
+    #imp_mean_01 = imputers('dataset_missing01.csv')
+    #imp_mean_01.impute(type = 'mean',missing = '01')
+    #imp_mean_10 = imputers('dataset_missing10.csv')
+    #imp_mean_10.impute(type = 'mean',missing = '10')
     imp_hd_01 = imputers('dataset_missing01.csv')
     imp_hd_01.impute(type = 'hd',missing = '01')
     imp_hd_10 = imputers('dataset_missing10.csv')
